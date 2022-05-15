f06def802aa3820a09fbd1fc93f1393209348901
brianyeo <brianyeo1995@gmail.com>
2022-05-15 22:10:14 +0800

    installed ts CRA

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT ?? 4000;
 const path = require("path");
+const cors = require("cors");
+
+//****************MIDDLEWARE***************//
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:4000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.use(express.json()); //to access req.body
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+console.log(__dirname);
+
+//****************ROUTES***************//
+// app.use('/daybits/journal', journalController);
+// app.use('/daybits/register', usersController);
+// app.use('/daybits/comments', CommentsController);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World" });
