7ad1c50130413903c44c30afa108b04f44cc39b6
brianyeo <brianyeo1995@gmail.com>
2022-05-19 21:13:18 +0800

    added navbar and react router

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      blue: "#1fb6ff",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a2",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   plugins: [],
