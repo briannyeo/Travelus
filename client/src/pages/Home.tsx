f1afc20d0eca1658019fcc351b6563ca9e29e53c
brianyeo <brianyeo1995@gmail.com>
2022-05-21 16:04:04 +0800

    testing route

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const createAccount = (userInfo) => {
+    fetch("/api/register/seedaccount", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //body: JSON.stringify(userInfo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("paginated post", data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={createAccount}>Click</button>
+    </div>
+  );
 };
 
 export default Home;
