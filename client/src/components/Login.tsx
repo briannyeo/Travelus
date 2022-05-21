45785e1bdb8a09eca08c0a9348c4cac08b567d18
brianyeo <brianyeo1995@gmail.com>
2022-05-21 09:59:39 +0800

    working on Login

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Login = () => {
+  //Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1",
+    },
+    {
+      username: "user2",
+      password: "pass2",
+    },
+  ];
+};
+
+export default Login;
