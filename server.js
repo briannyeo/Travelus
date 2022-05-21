b86934c1c228e8700a94994a59d0085c47ce02b4
brianyeo <brianyeo1995@gmail.com>
2022-05-21 08:08:08 +0800

    testing routes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 //****************DEPENDENCIES***************
 require("dotenv").config();
-import { PrismaClient } from "@prisma/client";
 
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT ?? 4000;
 const path = require("path");
 const cors = require("cors");
-const prisma = new //****************MIDDLEWARE***************//
+const prisma = new PrismaClient();
+
+//****************MIDDLEWARE***************//
 app.use(
   cors({
     credentials: true,
@@ -28,9 +30,21 @@ console.log("dirname:", __dirname);
 // app.use('/daybits/register', usersController);
 // app.use('/daybits/comments', CommentsController);
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello World!" });
+// app.get("/", (req, res) => {
+//   res.json({ msg: "Hello World!" });
+// });
+
+//create a job
+app.post("/", async (req, res) => {
+  const jobs = await prisma.jobs.findMany();
+  res.json({ jobs });
 });
+
+//get a job
+app.get("/:job_id", (req, res) => {});
+
+//delete a job
+app.get("/:job_id", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
