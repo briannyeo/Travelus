4f50b6abc25d1a4e123679f78c8f817f4455f83d
brianyeo <brianyeo1995@gmail.com>
2022-05-22 16:07:11 +0800

    added daisyui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,29 +1,58 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#6419E6",
-
-          secondary: "#D926A9",
-
-          accent: "#1FB2A6",
-
-          neutral: "#191D24",
-
-          "base-100": "#2A303C",
-
-          info: "#3ABFF8",
-
-          success: "#36D399",
-
-          warning: "#FBBD23",
-
-          error: "#F87272",
-        },
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#64bce1",
+      purple: "#7e5bef",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
       },
-    ],
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [require("daisyui")],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       mytheme: {
+  //         primary: "#6419E6",
+
+  //         secondary: "#D926A9",
+
+  //         accent: "#1FB2A6",
+
+  //         neutral: "#191D24",
+
+  //         "base-100": "#2A303C",
+
+  //         info: "#3ABFF8",
+
+  //         success: "#36D399",
+
+  //         warning: "#FBBD23",
+
+  //         error: "#F87272",
+  //       },
+  //     },
+  //   ],
+  // },
+  // plugins: [require("daisyui")],
 };
