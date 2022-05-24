f6fb0ca600d82380d4d6ebfa65f7f8f279d2f3c0
brianyeo <brianyeo1995@gmail.com>
2022-05-24 21:41:44 +0800

    added JWT

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,6 +5,7 @@ const express = require("express");
 const users = express.Router();
 const jwt = require("jsonwebtoken");
 const path = require("path");
+require("dotenv").config();
 
 //seed account
 // users.post("/seedaccount", async (req, res) => {
@@ -25,7 +26,7 @@ const path = require("path");
 //           username: "admin",
 //           password: bcrypt.hashSync("88888", 10),
 //           location_based: "Thailand",
-//           description: "admin description",
+//           //description: "admin description",
 //         },
 //       ],
 //     });
@@ -37,72 +38,104 @@ const path = require("path");
 //   }
 // });
 
-//Adds new users accounts to Prisma
+//ADD NEW USER TO PRISMA (REGISTER)
 users.post("/register", async (req, res) => {
-  console.log("req.body: ", req.body);
-  console.log("password: ", req.body.password); // correct
-  req.body.password = bcrypt.hashSync(
-    req.body.password,
-    bcrypt.genSaltSync(10)
-  );
-  console.log("password: ", req.body.password); // correct
-  // try {
-  //   const createdUser = await prisma.user.create({ data: req.body });
-  //   console.log("created user is: ", createdUser);
-  //   if (createdUser) {
-  //     res.status(200).json({ status: "success" });
-  //     return;
-  //   }
-  //   //res.redirect('/');
-  // } catch (err) {
-  //   res.status(500).json({ status: "failed" });
-  //   console.log(err);
-  // }
+  // console.log("req.body: ", req.body);
+  //console.log("password: ", req.body.password); // correct
+
+  try {
+    // Get user input
+    const { username, password, location_based } = req.body;
+
+    // Validate if user exist in our database
+    const oldUser = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+    //Encrypting user password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    //Create user in database
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        password: encryptedPassword,
+        location_based: location_based,
+      },
+    });
+
+    //create token
+    const accessToken = jwt.sign(user, process.env.TOKEN_SECRET, {
+      expiresIn: "2h",
+    });
+    //save user token
+    user.accessToken = accessToken;
+    //return new user
+    res.status(201).json({ status: "success" });
+    console.log("created user", user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-//login
-users.post("/login", (req, res) => {
-  //authenticate user
-  const { username, password } = req.body;
-  console.log(username, password);
+//LOGIN  AUTHENTICATION
+users.post("/login", async (req, res) => {
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { username, password } = req.body;
 
-  const payload = { username };
-  const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-  res.json({ accessToken: accessToken });
+    // Validate user input
+    if (!(username && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(user, process.env.TOKEN_SECRET, {
+        expiresIn: "2h",
+      });
+
+      // save user token
+      user.token = token;
+      console.log(token);
+
+      // user
+      res.status(200).json({ status: "success" });
+    } else {
+      res.status(400).send("Invalid Credentials");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
 });
+
+//LOGOUT
+users.post("/logout", (req, res) => {});
 
 //middleware to authenticate user
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403); //token not valid
 
     req.user = user;
     next();
   });
-
-  // try {
-  //   const token = req.headers.authorization.split(" ")[1];
-  //   const payload = jwt.verify(token, process.env.JWT_SECRET);
-  //   req.payload = payload;
-  //   next();
-  // } catch (error) {
-  //   res.send(403).end();
-  // }
 };
-
-//   if (username === "admin" && password === "admin") {
-//     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-//       expiresIn: "1h",
-//     });
-//     res.json({ token: token });
-//   } else {
-//     res.status(403).json({ msg: "Invalid credentials" });
-//   }
-// });
 
 module.exports = users;
