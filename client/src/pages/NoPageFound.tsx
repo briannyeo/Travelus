b46b0646f116b829c6828e8fb3116d909b52d015
brianyeo <brianyeo1995@gmail.com>
2022-05-25 11:02:48 +0800

    added no page found

---
+++ client/src/pages/NoPageFound.tsx
@@ -0,0 +1,10 @@
+const NoPageFound = () => {
+  return (
+    <h1>
+      The page does not exist! If you're not logged in, try logging in and come
+      back!
+    </h1>
+  );
+};
+
+export default NoPageFound;
