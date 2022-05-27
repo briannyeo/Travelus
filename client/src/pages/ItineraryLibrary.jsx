6b54f41dabe844aef78aa785570b01c6e63c91a9
brianyeo <brianyeo1995@gmail.com>
2022-05-27 23:50:28 +0800

    get all particular country's itineraries

--- client/src/pages/ItineraryLibrary.jsx
+++ client/src/pages/ItineraryLibrary.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 import { Combobox } from '@headlessui/react'
 import countries from '../data/countries.json'
@@ -10,28 +10,59 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function ItineraryLibrary() {
   const [query, setQuery] = useState('')
   const [selectedCountry, setSelectedCountry] = useState()
+  const [countryItinerary, setCountryItinerary] = useState({})
+  
   console.log(selectedCountry)
-
+  console.log(countryItinerary)
+  
+  // useEffect(() => {
+  //   const showItineraries = () => {
+  //     fetch("/api/itineraries/searchcountry", {
+  //       method: "POST",
+  //       credentials: "include",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(selectedCountry.name),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setCountryItinerary(data);
+  //       })
+  //       .catch((error) => console.log(error));
+  //   };
+  //   showItineraries();
+  // }, [selectedCountry]);
+  
   const filteredCountry =
     query === ''
       ? countries
       : countries.filter((country) => {
           return country.name.toLowerCase().includes(query.toLowerCase())
         })
+  
+        
+        
 
   return (
+    <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
+    <div className="header text-center text-5xl font-bold mt-5">
+    Explore travel guides and itineraries
+    </div>
     <Combobox as="div" value={selectedCountry} onChange={setSelectedCountry}>
-      <Combobox.Label className="block text-sm font-medium text-gray-700">Search for a destination</Combobox.Label>
+      <Combobox.Label className="max-w-screen-sm block text-sm font-medium text-gray-700">Search for a destination</Combobox.Label>
       <div className="relative mt-1">
+     
         <Combobox.Input
           className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
           onChange={(event) => setQuery(event.target.value)}
           //displayValue={(country) => country.name }
-          placeholder="Country"
         />
+        
+        
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
         </Combobox.Button>
@@ -71,5 +102,12 @@ export default function Example() {
         )}
       </div>
     </Combobox>
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div className="header text-left text-2xl font-bold mt-20">
+    Recent Itineraries
+    </div>
+    </div>
+
+    </div>
   )
 }
--- controllers/ItineraryController.js
+++ controllers/ItineraryController.js
@@ -2,5 +2,17 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const express = require("express");
 const itineraries = express.Router();
+const authenticateToken = require("../utils/auth");
+const jwt = require("jsonwebtoken");
+
+itineraries.get("/searchcountry", authenticateToken, async (req, res) => {
+  console.log(req.body);
+  const itineraries = await prisma.itineraries.findMany({
+    where: {
+      destination: req.body.country,
+    },
+  });
+  res.status(200).json({ itineraries });
+});
 
 module.exports = itineraries;
